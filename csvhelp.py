b29c34a8f414d31c9a9c3123060ae7b31b146b53
Seyi Obaweya <seyi_obaweya@yahoo.com>
2020-03-10 06:34:48 +0800

    csv.reader helper

--- csvhelp.py
+++ csvhelp.py
@@ -1,9 +1,25 @@
-from csv import DictReader, reader, DictWriter, writer
+import csv
 
 
 Class CsvHelperModule:
+    def printError(filename):
+        print('%s not found' %filename)
+
     def readCsvAsDict(self, filename):
-        with open(filename, 'r', encoding="utf8") as csvFile:
-            reader = csv.DictReader(csvFile)
-            print('Opening File %s' %filename)
-            return list(reader), reader.fieldnames      
+        try:
+            with open(filename, 'r', encoding="utf8") as csvFile:
+                reader = csv.DictReader(csvFile)
+                print('Opening File %s' %filename)
+                return list(reader), reader.fieldnames
+        except FileNotFoundError:
+            printError(filename)  
+
+    def readCsvAsList(self, filename):
+        try:
+            with open(filename, 'r', encoding="utf8") as csvFile:
+                reader = csv.reader(csvFile)
+                content = list(reader)
+                header = content.pop(0)
+                return content, header
+        except FileNotFoundError:
+            printError(filename)
